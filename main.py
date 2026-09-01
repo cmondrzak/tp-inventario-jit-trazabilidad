16f45e592515ff1867d60dd84aca1d19f83ac398
Lucas Aouad <laouad@itba.edu.ar>
2026-09-01 18:45:29 -0300

    nombre del commit

--- main.py
+++ main.py
@@ -24,6 +24,9 @@ class Material:
         
     def validar_reposicion(self):
         return
+    def validarid():
+
+        return
 
     
 
@@ -49,6 +52,11 @@ class Remesa:
     
     def movimientos(self):
         return
+    def validarid():
+        return
+    @staticmethod
+    def validarMayorCero(num):
+        return
 
 
 class Renglon:
@@ -59,6 +67,9 @@ class Renglon:
         
     def subtotal_renglon(self):
         return self.cantidad * self.precio_unitario
+    def validarid():
+        return
+
 
 class Pedido:
     def __init__(self, id_pedido, proveedor, renglones):
@@ -68,6 +79,8 @@ class Pedido:
         
     def agregar_renglon(self, renglon):
         self.renglones.append(renglon)
+    def validarid():
+        return
         
 class Proveedor:
     def __init__(self, id_proveedor, nombre, plazo_entrega):
@@ -77,11 +90,15 @@ class Proveedor:
         
     def entregar_material(self):
         return
+    def validarid():
+        return
 
 class Movimiento:
     def __init__(self,id_movimiento, fecha):
         self.id_movimiento = id_movimiento
         self.fecha = fecha
+    def validarid():
+        return
 
     
     
@@ -110,7 +127,8 @@ class RenglonRetiro:
     
 
 class Deposito:
-    def __init__(self, remesas, retiros, politicas, materiales, proveedores, movimientos):
+    def __init__(self,id_deposito ,remesas, retiros, politicas, materiales, proveedores, movimientos):
+        self.id_deposito=id_deposito
         self.remesas = remesas
         self.retiros = retiros
         self.politicas = politicas
@@ -126,6 +144,8 @@ class Deposito:
     def registrar_proveedor(self, id_proveedor, nombre, plazo_de_entrega):
         p = Proveedor(id_proveedor, nombre, plazo_de_entrega)
         self.proveedores.append(p)
+    def validarid():
+        return
         
     def existencia_fisica(self):
         return
