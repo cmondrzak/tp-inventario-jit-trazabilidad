4595dbaf2ca6291f51bb2db7fbabfcb5df3fede2
jolicastro-beep <licajoa@gmail.com>
2026-09-08 17:53:53 -0300

    Cambio validaciones

--- main.py
+++ main.py
@@ -167,4 +167,15 @@ class Deposito:
     
     def almacenar_remesa(self):
         return
+
+
+#Validaciones
+class Validaciones:
+    @staticmethod
+    def es_positivo(valor):
+        return valor is not None and valor > 0
+
+    @staticmethod
+    def es_no_vacio(texto):
+        return texto is not None and texto.strip() != ""
     
